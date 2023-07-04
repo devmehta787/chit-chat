9578ceb04c74127e44c90088dd477d3ba323ae77
devmehta787 <devmehta787@gmail.com>
2023-07-04 16:01:33 +0530

    sidebar complete

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -3,8 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss']
+  styleUrls: ['./sidebar.component.scss'],
 })
 export class SidebarComponent {
-
+  // conversations = [
+  //   { name: 'Abc Def', time: '3:00 PM', message: 'Hello' },
+  //   { name: 'xyz Def', time: '3:00 PM', message: 'Hello' },
+  //   { name: 'abc Def', time: '3:00 PM', message: 'Hello' },
+  //   { name: 'Abc Def', time: '3:00 PM', message: 'Hello' },
+  //   { name: 'xyz Def', time: '3:00 PM', message: 'Hello' },
+  //   { name: 'abc Def', time: '3:00 PM', message: 'Hello' },
+  //   { name: 'Abc Def', time: '3:00 PM', message: 'Hello' },
+  //   { name: 'xyz Def', time: '3:00 PM', message: 'Hello' },
+  //   { name: 'abc Def', time: '3:00 PM', message: 'Hello' },
+  //   { name: 'Abc Def', time: '3:00 PM', message: 'Hello' },
+  //   { name: 'xyz Def', time: '3:00 PM', message: 'Hello' },
+  //   { name: 'abc Def', time: '3:00 PM', message: 'Hello' },
+  // ];
 }
